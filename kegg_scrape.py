7c38ef8b3bea9eda36d9f4f8f72577cfcb890c3f
AntiCakejesCult <jessemittertreiner@gmail.com>
2023-10-17 05:19:52 +0000

    some syntax changes and overall cleaning of the script.

--- kegg_scrape.py
+++ kegg_scrape.py
@@ -1,13 +1,20 @@
 import pandas as pd
 import Bio.KEGG.REST as kegg
-import requests
 import json
+import os
 
-test_dictionary = {}
+filtered_kegg = {}
 
+def get_file():
+    # Can filter for other files is necessary
+    current_cwd = os.getcwd()
+    kegg_file = os.listdir(os.path.join(current_cwd, "input"))
+    return os.path.join(current_cwd, "input", kegg_file[0])
+    
+    
 
 def parse_gene_data(data):
-    gene_dict = {}
+    kegg_dict = {}
     current_key = ""
     for line in data.split("\n"):
         if line.startswith("///"):
@@ -17,22 +24,20 @@ def parse_gene_data(data):
         value = value.strip()
         if key:
             current_key = key
-        if current_key in gene_dict:
-            if isinstance(gene_dict[current_key], list):
-                gene_dict[current_key].append(value)
+        if current_key in kegg_dict:
+            if isinstance(kegg_dict[current_key], list):
+                kegg_dict[current_key].append(value)
             else:
-                gene_dict[current_key] = [gene_dict[current_key], value]
+                kegg_dict[current_key] = [kegg_dict[current_key], value]
         else:
-            gene_dict[current_key] = value
-    return gene_dict
+            kegg_dict[current_key] = value
+    return kegg_dict
 
 
 def get_info(sub_list):
     pathways = {}
-    
     if isinstance(sub_list, str):
         sub_list = [sub_list]
-        
     for element in sub_list:
         try:
             id, description = element.split(" ", 1)
@@ -42,34 +47,31 @@ def get_info(sub_list):
     return pathways
 
 
+def add_info(id, kegg_variable, kegg_dict):
+        if id in filtered_kegg:
+            filtered_kegg[id].update({kegg_variable: kegg_dict})
+        else:
+            filtered_kegg[id] = {kegg_variable: kegg_dict}
+    
 
 def get_gene_symbol(kegg_info, id):
     gene_symbol_dict = kegg_info.get("SYMBOL", -1)
     if gene_symbol_dict != -1:
         symbol = [i.strip() for i in gene_symbol_dict.split(",")]
-        if id in test_dictionary:
-            test_dictionary[id].update({"symbol": symbol})
-        else:
-            test_dictionary[id] = {"symbol": symbol}
+        add_info(id, "symbol", symbol)
 
 def get_name(kegg_info, id):
     name_dict = kegg_info.get("NAME", -1)
     if name_dict != -1:
         description = get_info(name_dict)
         description = description.pop('(RefSeq)', None)
-        if id in test_dictionary:
-            test_dictionary[id].update({"description": description})
-        else:
-            test_dictionary[id] = {"description": description}
+        add_info(id, "description", description)
 
 def get_pathway(kegg_info, id):
     pathway_dict = kegg_info.get("PATHWAY", -1)
     if pathway_dict != -1:
         pathway = get_info(pathway_dict)
-        if id in test_dictionary:
-            test_dictionary[id].update({"pathway": pathway})
-        else:
-            test_dictionary[id] = {"pathway": pathway}
+        add_info(id, "pathway", pathway)
 
 
 def get_brite(kegg_info, id):
@@ -83,14 +85,18 @@ def get_brite(kegg_info, id):
                 brite[header] = []
             else:
                 brite[header].append(element)
-            if id in test_dictionary:
-                test_dictionary[id].update({"brite": brite})
-            else:
-                test_dictionary[id] = {"brite": brite} 
-                
+            add_info(id, "brite", brite)    
 
-if __name__ == "__main__":
-    df = pd.read_excel("Copy of KEGG_BRITE_AxLN-spleen.xlsx")
+
+            
+
+def write_kegg_data():
+    with open("pathway.json", "w") as f:
+        json.dump(filtered_kegg, f, indent=4)
+        
+
+def main():
+    df = pd.read_excel(get_file())
     ids = df.iloc[:, 0].dropna()
     for id in ids:
         if id.startswith("hsa"):
@@ -102,7 +108,8 @@ if __name__ == "__main__":
             get_name(gene_dict, id)
             get_pathway(gene_dict, id)
             get_brite(gene_dict, id)
-            
+
+if __name__ == "__main__":
+    main()
+    write_kegg_data()
         
-    with open("pathway.json", "w") as f:
-        json.dump(test_dictionary, f, indent=4)
